9e046785d195212192e53210b540ae8679364af6
Mariia Kucheieva <mary.kucheeva@gmail.com>
2023-10-14 20:23:01 +0300

    just save

--- src/app/(main)/(routes)/dashboard/page.tsx
+++ src/app/(main)/(routes)/dashboard/page.tsx
@@ -1,49 +1,20 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import Image from "next/image";
 import React from "react";
-import IconButton from "../../_components/icon-button";
-import {
-  ChevronsLeft,
-  ChevronsRight,
-  MenuIcon,
-  PlusCircle,
-} from "lucide-react";
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 import { useUser } from "@clerk/nextjs";
 
-type DashboardPageProps = {
-  shiftRight: boolean;
-  setShiftRight: React.Dispatch<React.SetStateAction<boolean>>;
-};
+import { PlusCircle } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
-const DashboardPage = ({ shiftRight, setShiftRight }: DashboardPageProps) => {
+const DashboardPage = () => {
   const { user } = useUser();
 
   return (
     <>
       <div className="flex flex-col items-center justify-start p-2 md:p-5">
-        {/* <IconButton
-          className={cn(
-            "z-100 fixed right-0 top-0 ml-auto md:hidden",
-            shiftRight ? "translate-x-52" : "translate-x-0",
-          )}
-          onClick={() => setShiftRight(true)}
-        >
-          <MenuIcon
-            className={cn(
-              "duration-400 h-6 w-6 text-black text-muted-foreground transition-transform dark:text-slate-100",
-            )}
-          />
-        </IconButton> */}
-
-        <div
-          className={cn(
-            "flex h-full flex-col items-center justify-center space-y-4 p-2  md:p-5",
-            shiftRight ? "translate-x-52" : "translate-x-0",
-          )}
-        >
+        <div className="flex h-full flex-col items-center justify-center space-y-4 p-2  md:p-5">
           <Image
             src="/new_note.svg"
             height="300"
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { Sidebar } from "./_components/sidebar";
 import { useState } from "react";
+import { Sidebar } from "./_components/sidebar";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const [showSidebar, setShowSidebar] = useState(true);
 
   return (
     <div className="relative flex h-screen w-screen flex-row bg-background dark:bg-[#1f1f1f]">
-      <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+      {/* <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} /> */}
       <main className="h-full flex-1 overflow-y-auto">{children}</main>
     </div>
   );
