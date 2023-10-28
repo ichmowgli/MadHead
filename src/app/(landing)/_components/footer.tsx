a6b0b3f0d43af472feaf15437e6e7d66448911f4
Mara <mariia.kucheieva@gmail.com>
2023-10-28 17:33:20 +0300

    fix footer

--- src/app/(landing)/_components/footer.tsx
+++ src/app/(landing)/_components/footer.tsx
@@ -2,15 +2,10 @@ import { Button } from '@/components/ui/button';
 
 export const Footer = () => {
   return (
-    <div className='z-50 flex w-full items-center bg-background p-6 dark:bg-[#1f1f1f]'>
-      <div className='flex w-full items-center justify-between gap-x-2 text-muted-foreground md:ml-auto md:justify-end'>
-        <Button variant='ghost' size='sm'>
-          Privacy Policy
-        </Button>
-        <Button variant='ghost' size='sm'>
-          Terms & Conditions
-        </Button>
-      </div>
+    <div className='z-50 flex w-full items-center justify-center bg-background p-6 dark:bg-[#1f1f1f]'>
+        <p className='text-sm font-medium'>{new Date().getFullYear()} {""}
+          <a className="hover:text-green-600 font-bold" href="https://github.com/ichmowgli/">@ichmowgli</a>
+        </p>
     </div>
   );
 };
