8a3d4e4bc05a680bc1064f006cfdf4f60557344f
Mara <mariia.kucheieva@gmail.com>
2023-10-29 13:32:25 +0200

    add "All Notes" view

--- src/app/(main)/(routes)/notes/page.tsx
+++ src/app/(main)/(routes)/notes/page.tsx
@@ -18,6 +18,7 @@ const DashboardPage = () => {
   const { user } = useUser();
   const router = useRouter();
   const { addNote } = useNoteStore();
+  const notes = useNoteStore((s) => s.notes);
 
   const handleCreate = () => {
     const promise = addNote({
@@ -34,9 +35,9 @@ const DashboardPage = () => {
     });
   };
 
-  return (
-    <>
-      <div className=' flex h-full flex-col items-center justify-center space-y-4  p-2 md:p-5'>
+  if (notes.length === 0)
+    return (
+      <div className=' flex h-full flex-col items-center justify-center space-y-4'>
         <Image
           src='/new_note.svg'
           height='300'
@@ -62,6 +63,34 @@ const DashboardPage = () => {
           <PlusCircle className='mr-2 h-4 w-4' />
           Create a new note
         </Button>
+      </div>
+    );
+
+  return (
+    <>
+      <div className='flex h-full flex-col gap-y-4 px-4 pt-12 md:px-12 '>
+        <h2 className='break-words text-left text-2xl font-bold text-[#3F3F3F] dark:text-[#CFCFCF] md:text-3xl lg:text-5xl'>
+          Your Notes
+        </h2>
+
+        <div className='grid grid-cols-1 gap-3 sm:grid-cols-3 md:grid-cols-5'>
+          {notes.map((note) => (
+            <a href={`/notes/${note.id}`} key={note.id}>
+              <div className='flex flex-col overflow-hidden rounded-lg border border-muted-foreground bg-secondary transition hover:-translate-y-1 hover:shadow-xl'>
+                <div className='p-4'>
+                  <h3 className='truncate text-xl font-semibold text-[#3F3F3F] dark:text-[#CFCFCF]'>
+                    {note.title}
+                  </h3>
+                  <div className='h-1'></div>
+                  <p className='text-sm text-gray-500'>
+                    Updated at: <br />
+                    {new Date(note.updatedAt).toLocaleDateString()}
+                  </p>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
       </div>
     </>
   );
